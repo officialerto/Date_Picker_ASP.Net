184d9bbf91d30d74ce3833ea4ae1dcc93b699b14
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-01-04 17:01:17 +0300

    Add project files.

---
+++ Date_Picker_MVC/Controllers/DatePickerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Date_Picker_MVC.Controllers
+{
+    public class DatePickerController : Controller
+    {
+        // GET: DatePicker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Date_Picker_MVC/Models/DatePickerModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Date_Picker_MVC.Models
+{
+    public class DatePickerModel
+    {
+        public DateTime dtmDate { get; set; }
+    }
+}
